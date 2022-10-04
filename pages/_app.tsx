a78e4aa67e91deaec2849125cbcf61d30d16473f
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-10-04 21:12:11 +0200

    feat: add manifest and favicons

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,7 +15,41 @@ function App({ Component, pageProps }: AppProps) {
                     name="description"
                     content="Test if you're qualified to join the LAPD"
                 />
-                <link rel="icon" href="/favicon.ico" />
+
+                <link
+                    rel="apple-touch-icon"
+                    sizes="72x72"
+                    href="/favicon/apple-icon-72x72.png"
+                />
+                <link
+                    rel="apple-touch-icon"
+                    sizes="144x144"
+                    href="/favicon/apple-icon-144x144.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="192x192"
+                    href="/favicon/android-icon-192x192.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="36x36"
+                    href="/favicon/favicon-36x36.png"
+                />
+                <link
+                    rel="icon"
+                    type="image/png"
+                    sizes="96x96"
+                    href="/favicon/favicon-96x96.png"
+                />
+                <link
+                    rel="shortcut icon"
+                    type="image/x-icon"
+                    href="/favicon/favicon.png"
+                />
+                <link rel="manifest" href="/static/manifest.json" />
             </Head>
             <HeaderComp />
             <Component {...pageProps} />
