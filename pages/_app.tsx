6808b7f38b53b54d45e3c94d4fac9b8715efd458
N0chteil <53608074+N0chteil@users.noreply.github.com>
2022-10-04 19:38:17 +0200

    feat: update app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import Head from "next/head";
+
+import { HeaderComp } from "../components/HeaderComp";
+
+import "../styles/globals.css";
+
+function App({ Component, pageProps }: AppProps) {
+    return (
+        <>
+            <Head>
+                <title>LAPD One</title>
+                <meta
+                    name="description"
+                    content="Test if you're qualified to join the LAPD"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <HeaderComp />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default App;
